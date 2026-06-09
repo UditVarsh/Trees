d9805abbc19f372677deb01085597fcb94601ece
Udit <lakshyaudit@gmail.com>
2026-06-09 12:41:30 +0530

    added children sumproperty

---
+++ trees/L30_chidren_sum_property.cpp
@@ -0,0 +1,71 @@
+#include <algorithm>
+#include <bits/stdc++.h>
+#include <cstddef>
+#include <stack>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+struct Node_tree{
+    int data;
+    Node_tree* left;
+    Node_tree* right;
+
+    Node_tree(int data){
+        this->data = data;
+        left = nullptr;
+        right =nullptr;
+    }
+};
+void printTree(Node_tree *root, string indent = "", bool isLeft = true) {
+  if (root == nullptr)
+    return;
+
+  // Print right subtree first
+  printTree(root->right, indent + (isLeft ? "│   " : "    "), false);
+
+  // Print current node
+  cout << indent;
+
+  if (isLeft)
+    cout << "└── ";
+  else
+    cout << "┌── ";
+
+  cout << root->data << endl;
+
+  // Print left subtree
+  printTree(root->left, indent + (isLeft ? "    " : "│   "), true);
+}
+void children_sum(Node_tree*root){
+    if(!root)return;
+    if(root->left)root->left->data =root->data;
+    if(root->right)root->right->data=root->data;
+    children_sum(root->left);
+    children_sum(root->right);
+    int sum=0;
+    if(!root->left&&!root->right)return;
+    if(root->left)sum+=root->left->data;
+    if(root->right)sum+=root->right->data;
+    root->data=sum;
+
+}
+int main() {
+  Node_tree root = Node_tree(5);
+  root.left = new Node_tree(6);
+  root.right = new Node_tree(7);
+  root.left->left = new Node_tree(8);
+  root.left->right = new Node_tree(9);
+  root.left->right->left = new Node_tree(1);
+  printTree(&root);
+children_sum(&root);
+printTree(&root);
+
+  cout<<endl<<endl;
+  
+  cout<<endl<<endl;
+//   cout<<maxheight_opt(&root);
+  cout<<endl;
+
+}
